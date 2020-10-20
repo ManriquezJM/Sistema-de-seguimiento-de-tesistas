56911418e9f4cd30e70eb8fc156ea7ccdcd1c2a6
ManriquezJM <manriquez.josematias@gmail.com>
2020-10-20 14:03:51 -0300

    fix localstorage

--- resources/js/components/Home.vue
+++ resources/js/components/Home.vue
@@ -6,7 +6,26 @@
 
 <script>
 export default {
-
+   mounted() {
+            this.getRefrescarUsuarioAutenticado();
+        },
+        methods: {
+            getRefrescarUsuarioAutenticado() {
+                if (this.$router.currentRoute.name != 'login') {
+                    var url = '/authenticate/getRefrescarUsuarioAutenticado'
+                    axios.get(url).then(response => {
+                    }).catch(error => {
+                        if (error.response.status == 401) {
+                          if(localStorage['authUser']){
+                            this.$router.push({name: 'login'})
+                            location.reload();
+                            localStorage.clear();
+                          }
+                        }
+                    })
+                }
+            },
+        },
 }
 </script>
 
